2e201e35c3a8ef2c1a5b21f7fc9fcca87a8a6056
algakz06 <77662392+algakz06@users.noreply.github.com>
2023-08-15 00:55:19 +0300

    db.refresh() was deleted cause error

--- src/app/core/crud.py
+++ src/app/core/crud.py
@@ -1,8 +1,7 @@
-from sqlalchemy import update
+from typing import Optional
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import NoResultFound
 
-from typing import Union
 
 from app.models import db_models, models
 from app.config import log
@@ -23,6 +22,7 @@ def get_user(db: Session, tg_user_id: int) -> models.User | None:
     )
     if not user:
         return None
+    log.info(f"User {user} was found in db")
     return models.User.from_orm(user)
 
 
@@ -31,7 +31,56 @@ def add_user_to_db(db: Session, tg_user_id: int):
     db_user = db_models.Users(tg_id=tg_user_id)
     db.add(db_user)
     db.commit()
-    db.refresh(db_user)
+    log.info(f"User {db_user} was added to db")
+
+
+# endregion
+
+
+# region Notion
+def add_notion_token(
+    db: Session,
+    tg_user_id: int,
+    token: Optional[str] = "",
+):
+    """adding notion token to db"""
+    db_user = (
+        db.query(db_models.Users)
+        .filter(
+            db_models.Users.tg_id == tg_user_id,
+        )
+        .first()
+    )
+    if not db_user:
+        raise NoResultFound
+    db_user.notion_token = token
+    db.commit()
+    log.info(f"User {db_user} was added notion token to db")
+
+
+def add_notion_db(db: Session, tg_user_id: int, db_id: str, db_name: str):
+    """adding notion db to db"""
+    db_user = (
+        db.query(db_models.Users)
+        .filter(
+            db_models.Users.tg_id == tg_user_id,
+        )
+        .first()
+    )
+
+    if not db_user:
+        raise NoResultFound
+
+    db_db = db_models.Databases(
+        db_id=db_id,
+        db_name=db_name,
+        user_id=db_user.id,
+    )
+
+    db.add(db_db)
+    db.commit()
+
+    log.info(f"User {db_user} added notion db: {db_id} to db")
 
 
 # endregion
